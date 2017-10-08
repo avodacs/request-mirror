6f330e88cde9b3d3a0599aa6646fbbc0c563dcea
Erick Pece <erick@pece.me>
2017-10-08 15:32:17 -0400

    Hide HAProxy headers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.0.1] - UNRELEASED
+### Changed
+- `x-forwarded-port`, `x-forwarded-for`, and `connection` headers are not displayed on inspection, to prevent showing headers added by HAProxy.
+
 ## [1.0.0] - 2017-10-08
 ### Added
 - Initial project files
--- src/routes/inspect.js
+++ src/routes/inspect.js
@@ -4,25 +4,42 @@ const router = express.Router();
 const tokenHelper = require('../helpers/tokenHelper.js');
 
 /* Generate new token */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let id = tokenHelper.generateToken();
 
   res.redirect(`/inspect/${id}`);
 });
 
 /* Get inspection details */
-router.get(/\/(.{8})/, function(req, res, next) {
+router.get(/\/(.{8})/, function (req, res, next) {
   let id = req.params[0];
   let url = `${req.protocol}://${req.get('host')}/log/${id}`;
 
   let accept = req.headers.accept;
 
   if (accept === 'application/json') {
-    db.get(id, function(data) {
+    db.get(id, function (data) {
+      // Purge headers that we don't want to show
+      let hiddenHeaders = ['x-forwarded-port', 'x-forwarded-for', 'connection'];
+
+      data.forEach(function (record) {
+        let keys = Object.keys(record.headers);
+
+        hiddenHeaders.forEach(function (header) {
+          if (keys.indexOf(header) > -1) {
+            // Remove the key
+            delete record.headers[header];
+          }
+        });
+      });
+
       res.json(data);
     });
   } else {
-    res.render('inspect', { token: id, url: url });
+    res.render('inspect', {
+      token: id,
+      url: url
+    });
   }
 });
 
