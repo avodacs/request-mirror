e437ee2297f80d2bba22c89f8d144561bedf8c20
Erick Pece <erick@pece.me>
2017-10-08 23:25:29 -0400

    Request protocol pulls from x-forwarded-proto if available

--- src/routes/log.js
+++ src/routes/log.js
@@ -4,17 +4,23 @@ const moment = require('moment');
 const router = express.Router();
 
 /* Insert log record */
-router.all(/\/(.{8})/, function(req, res, next) {
+router.all(/\/(.{8})/, function (req, res, next) {
   let id = req.params[0];
+
+  let protocol = req.protocol;
+  if (req.headers['x-forwarded-proto'] !== undefined) {
+    protocol = req.headers['x-forwarded-proto'];
+  }
 
   db.insert(
     id,
-    `${req.protocol}://${req.get('host')}${req.originalUrl}`,
+    `${protocol}://${req.get('host')}${req.originalUrl}`,
     req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     req.method,
     req.headers,
     req.query,
-    req.body, function() {
+    req.body,
+    function () {
       res.send('ok');
     }
   );
